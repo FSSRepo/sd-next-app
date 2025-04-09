2b2c199775ff4c4fa40ba856bdf4e73cdf9825dd
FSSRepo <go778sgt@gmail.com>
2025-04-08 18:12:32 -0600

    improve README

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,8 @@
 export default function About() {
     return (
-        <h1 className="dark:text-white">Acerca de stable diffusion</h1>
-        
+        <div>
+            <h1 className="dark:text-white">Acerca de stable diffusion</h1>
+            <p>Modelo de diffusion</p>
+        </div>
     );
 }
